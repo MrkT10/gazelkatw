33d52801e79761180276318489d9a10a94fd0e57
Mateusz Markowski <markowski.dev@gmail.com>
2026-07-10 00:19:44 +0200

    set blank homepage template

---
+++ theme/gazelka-tailwind/theme/homepage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * This is the template for a custom Gazelka homepage
+ *
+ * Template name: Gazelka homepage
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Gazelka_Tailwind
+ */
+
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+			<hr>
+			<div class="pt-5 text-center">
+				<p>Pozostała treść strony</p>
+			</div>
+		</main><!-- #main -->
+	</section><!-- #primary -->
